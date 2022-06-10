228ad80a326d695f0ac7e698264c42022b8f857f
Jefferson Souza Rodrigues <jeffersoncacula@gmail.com>
2022-06-10 04:41:13 -0400

    colocando a primeira imagem na página

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,18 @@
 import styles from '../styles/Home.module.css'
 import Botao from '../components/Botao'
-
+import Image from 'next/image'
+import paradaDeMao from '../../public/paradaDeMao.png'
+import Wave from '../components/Wave'
 
 export default function Home() {
   
   return(
     <div className={styles.container}>
-      <h1>Jefferson é o melhor</h1>
-      <Botao  rota={'/obrigado'} >Página de Obrigado</Botao>
+      <div className={styles.content}>
+        <Botao  rota={'/obrigado'} >Página de Obrigado</Botao><p></p>
+        <Image src={paradaDeMao} className={styles.paradaDeMao}></Image>
+      </div>
+        <Wave></Wave>
     </div>
   )
 }
